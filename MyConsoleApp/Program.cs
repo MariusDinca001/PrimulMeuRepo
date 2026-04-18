5b0f8d981d7b3aa66473613431abd23632e69edf
MariusDinca001 <marius.n.dinca@gmail.com>
2026-04-18 17:01:11 +0300

    change shared line on main

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This example will be saved in my repo");
+Console.WriteLine("Mesaj din MAIN");
+Console.WriteLine("Linie comuna modificata din MAIN");
