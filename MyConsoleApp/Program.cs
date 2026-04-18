51acbd97b174802803cb8f9ded96315611f2b669
MariusDinca001 <marius.n.dinca@gmail.com>
2026-04-18 16:47:08 +0300

    add second message on feature branch

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This example will be saved in my repo");
+Console.WriteLine("Salut din main project!");
+Console.WriteLine("Acesta este branch-ul feature-mesaj.");
