e67b030af872e586b9f006923f277dca5e50e058
MariusDinca001 <marius.n.dinca@gmail.com>
2026-04-18 17:56:20 +0300

    modif 3

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("This example will be saved in my repo");
 Console.WriteLine("Mesaj din MAIN");
 Console.WriteLine("Acesta este branch-ul feature-mesaj.");
 Console.WriteLine("Modificare2 branch-ul feature-mesaj.");
+Console.WriteLine("Modificare3 branch-ul feature-mesaj.");
